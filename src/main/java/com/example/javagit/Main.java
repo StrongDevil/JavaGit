328047e070ed1c2172d0099f0f41db7cb098aadc
StrongDevil <sex.Bryansk@yandex.ru>
2023-02-20 20:32:23 +0600

    Test comit

--- src/main/java/com/example/javagit/Main.java
+++ src/main/java/com/example/javagit/Main.java
@@ -3,5 +3,6 @@ package com.example.javagit;
 public class Main {
     public static void main(String[] args) {
         System.out.println("huy");
+        System.out.println("huy2");
     }
 }
---
+++ src/main/java/com/example/javagit/Test.java
@@ -0,0 +1,4 @@
+package com.example.javagit;
+
+public class Test {
+}
